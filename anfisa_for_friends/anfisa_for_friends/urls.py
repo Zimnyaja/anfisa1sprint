913acaf502a83a085c30364bb929730e63fb81eb
Yuliya Tsaryk <koshkajt@gmail.com>
2024-07-13 21:02:02 +0300

    ended task

--- anfisa_for_friends/anfisa_for_friends/urls.py
+++ anfisa_for_friends/anfisa_for_friends/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('homepage.urls', namespace='homepage')),
-    path('about/', include('about.urls', namespace='about')),
-    path('ice_cream/', include('ice_cream.urls', namespace='ice_cream')),
-    path('admin/', admin.site.urls),
+    path('about', include('about.urls', namespace='about')),
+    path('ice_cream', include('ice_cream.urls', namespace='ice_cream')),
+    path('admin', admin.site.urls),
 ]
